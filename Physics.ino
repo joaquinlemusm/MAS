451c510377b4031c175ae4f9d15f35b7892e28f3
Aníbal Joaquín Lemus Martínez <91865286+joaquinlemusm@users.noreply.github.com>
2022-10-20 19:11:53 -0600

    Add files via upload

---
+++ Physics.ino
@@ -0,0 +1,52 @@
+const int ldrPin = A0;
+const int ledPin = 13;
+unsigned long startMillis;
+unsigned long currentMillis;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+  startMillis = millis();
+}
+
+void ledAction () {
+  int ldrStatus = analogRead(ldrPin);
+  if (ldrStatus <= 300) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  } 
+}
+
+void loop() {
+  int ldrStatus = analogRead(ldrPin);
+  if (ldrStatus <= 300) {
+    digitalWrite(ledPin, HIGH);
+    if (currentMillis - startMillis > 0) {
+      Serial.println("Tiempo: ");
+      Serial.println(currentMillis - startMillis);
+    }
+    startMillis = currentMillis;
+  } else {
+    digitalWrite(ledPin, LOW);
+    currentMillis = millis();
+  } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
